a69e0d603521995c8371841d4836312d6d21d003
httpdaniel <daniel.097@hotmail.com>
2020-11-01 02:24:10 +0000

    Update Movement

--- Assets/CanvasControl.cs
+++ Assets/CanvasControl.cs
@@ -7,16 +7,27 @@ public class CanvasControl : MonoBehaviour
 
     public float minX, maxX, minY, maxY;
 
+    Vector2 targetPosition;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        targetPosition = getRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = getRandomPosition();
+        }
     }
 
     Vector2 getRandomPosition()
