f0b73c4848b3de86f00192ba47042f950c90ab87
vaibhav0195 <vaibhav0195@gmail.com>
2020-11-03 13:13:13 +0000

    transistioning done

---
+++ Assets/JoinLobby.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinLobby : MonoBehaviour
+{
+    public void join()
+    {
+        Debug.Log("joining the lobby");
+    }
+}
--- Assets/createLobby.cs
+++ Assets/createLobby.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CreateLobby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void createLobby()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Creating the lobby");
+        bool status = true; // take this variable from the server
+        if (status == true)
+        {
+            //start game screen
+            Debug.Log("Starting game");
+        }
     }
 }
