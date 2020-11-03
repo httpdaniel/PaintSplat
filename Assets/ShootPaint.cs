68317525ec789c7115cbe3a65824edeeb1c38406
httpdaniel <daniel.097@hotmail.com>
2020-11-03 13:13:21 +0000

    Updating Shoot

--- Assets/ShootPaint.cs
+++ Assets/ShootPaint.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootPaint : MonoBehaviour
 {
     public int hitCounter;
+    public Text scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         hitCounter = 0;
+        scoreText.text = "Score: " + hitCounter;
     }
 
     // Update is called once per frame
     void Update()
     {
+        scoreText.text = "Score: " + hitCounter;
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hitCounter++;
+        }
     }
 }
