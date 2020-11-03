607c41ede75e4302cc12dcaddb54281fecb2f5fe
httpdaniel <daniel.097@hotmail.com>
2020-11-03 01:06:52 +0000

    Updating Joystick

--- Assets/Joystick.cs
+++ Assets/Joystick.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Joystick : MonoBehaviour
 {
-    private GameObject innercircle, outercircle;
+    public GameObject innercircle, outercircle;
     private Rigidbody2D rb;
     private float moveSpeed;
     private Touch oneTouch;
@@ -17,12 +17,66 @@ public class Joystick : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         outercircle.SetActive(false);
+        innercircle.SetActive(false);
+        moveSpeed = 6f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            oneTouch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(oneTouch.position);
 
+            switch (oneTouch.phase)
+            {
+                case TouchPhase.Began:
+                    outercircle.SetActive(true);
+                    innercircle.SetActive(true);
+
+                    outercircle.transform.position = touchPosition;
+                    innercircle.transform.position = touchPosition;
+
+                    break;
+
+                case TouchPhase.Stationary:
+                    moveDuck();
+
+                    break;
+
+                case TouchPhase.Moved:
+                    moveDuck();
+
+                    break;
+
+                case TouchPhase.Ended:
+                    outercircle.SetActive(false);
+                    innercircle.SetActive(false);
+
+                    rb.velocity = Vector2.zero;
+
+                    break;
+            }
+        }
+
+    }
+
+    private void moveDuck()
+    {
+        innercircle.transform.position = touchPosition;
+
+        innercircle.transform.position = new Vector2(
+            Mathf.Clamp(innercircle.transform.position.x,
+            outercircle.transform.position.x - 0.63f,
+            outercircle.transform.position.x + 0.63f),
+            Mathf.Clamp(innercircle.transform.position.y,
+            outercircle.transform.position.y - 0.63f,
+            outercircle.transform.position.y + 0.63f)
+        );
+
+        moveDirection = (innercircle.transform.position - outercircle.transform.position).normalized;
+        rb.velocity = moveDirection * moveSpeed;
     }
 }
