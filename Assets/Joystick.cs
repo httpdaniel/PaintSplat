d2526c9077a87593353d685a0dd49ab993a65f10
httpdaniel <daniel.097@hotmail.com>
2020-11-03 00:35:36 +0000

    Joystick Script

---
+++ Assets/Joystick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    private GameObject innercircle, outercircle;
+    private Rigidbody2D rb;
+    private float moveSpeed;
+    private Touch oneTouch;
+    private Vector2 touchPosition;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        outercircle.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
