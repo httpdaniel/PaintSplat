43e6f136e78ed02882349e79949de3033e06b128
vaibhav0195 <vaibhav0195@gmail.com>
2020-11-03 13:36:53 +0000

    getting username

--- Assets/createLobby.cs
+++ Assets/createLobby.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CreateLobby : MonoBehaviour
 {
+    public TMP_InputField username;
+    public TMP_InputField maxplayer;
     public void createLobby()
     {
+        
         Debug.Log("Creating the lobby");
         bool status = true; // take this variable from the server
         if (status == true)
         {
+            Debug.Log("User name"+username.text);
+            Debug.Log("max Players"+maxplayer.text);
             //start game screen
             Debug.Log("Starting game");
         }
