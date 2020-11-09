3976bd4590770645106129f8c052c542af60a29a
vaibhav <vaibhav0195@gmail.com>
2020-11-09 19:10:54 +0000

    Added userobject and queues

---
+++ Assets/PlayerQueue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerQueue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GetSocket socketObj ;
+    void Start()
+    {
+        socketObj = SocketFactory.getSocketForApp("127.0.0.1", 10500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
