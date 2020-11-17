2b13ac2376ed5cf9bd3c926ceca9e769bb9d9cac
httpdaniel <daniel.097@hotmail.com>
2020-11-17 01:52:05 +0000

    Update GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text winnerName;
+    [SerializeField]
+    public GameObject endScreen;
+
+    void Start() {
+        winnerName.text = "";
+        endScreen.SetActive(false);
+    }
     //public Text playerName;
+    void Update() {
+        //check time
+        // if time is up - end game
+    }
+
+    void setName() {
+        //winnerName.text = name;
+    }
 
     void endGame()
     {
         Debug.Log("Game Over");
+        // change text to winner
+        endScreen.SetActive(true);
     }
 }
